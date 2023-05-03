ea69c8633c5f9a49369d719c212c56103a773f99
imndx <imndxx@gmail.com>
2023-05-03 08:49:55 +0800

    initial commit

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
 import Vue from 'vue'
-import App from './App.vue'
+import Single from './voip/Single.vue'
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+    render: h => h(Single),
 }).$mount('#app')
---
+++ src/platform.js
@@ -0,0 +1,33 @@
+// 平台相关代码，目前主要用来处理electron 和 浏览器之间不同
+
+export function isElectron() {
+    // Renderer process
+    if (typeof window !== 'undefined' && typeof window.process === 'object' && window.process.type === 'renderer') {
+        return true;
+    }
+
+    // Main process
+    if (typeof process !== 'undefined' && typeof process.versions === 'object' && !!process.versions.electron) {
+        return true;
+    }
+
+    // Detect the user agent when the `nodeIntegration` option is set to true
+    if (typeof navigator === 'object' && typeof navigator.userAgent === 'string' && navigator.userAgent.indexOf('Electron') >= 0) {
+        return true;
+    }
+
+    return false;
+}
+
+// pc
+export const remote = null;
+export const ipcRenderer = null;
+export const ipcMain = null;
+export const fs = null;
+export const currentWindow = null;
+export const BrowserWindow = null;
+export const AppPath = null;
+export const desktopCapturer = null;
+
+// for web
+// export const PostMessageEventEmitter = require('./windowEmitter');
