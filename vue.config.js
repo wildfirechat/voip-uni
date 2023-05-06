78d4af5dbe41389d077f1a43781ac471fa221d88
imndx <imndxx@gmail.com>
2023-05-06 09:59:57 +0800

    编译成单独一个文件

---
+++ vue.config.js
@@ -0,0 +1,25 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const HtmlWebpackInlineSourcePlugin = require('html-webpack-inline-source-plugin');
+
+module.exports = {
+    css: {
+        extract: false,
+    },
+    configureWebpack: {
+        // externals 目前没生效
+        externals: {
+            client: /\/client\//,
+        },
+        optimization: {
+            splitChunks: false // makes there only be 1 js file - leftover from earlier attempts but doesn't hurt
+        },
+        plugins: [
+            new HtmlWebpackPlugin({
+                filename: 'voip-dist.html', // the output file name that will be created
+                template: 'src/output-template.html', // this is important - a template file to use for insertion
+                inlineSource: '.(js|css)$' // embed all javascript and css inline
+            }),
+            new HtmlWebpackInlineSourcePlugin(HtmlWebpackPlugin)
+        ]
+    },
+}
